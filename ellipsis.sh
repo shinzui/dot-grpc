bf523278bfa2effce31b2a841379f5d97b8be269
Nadeem Bitar <nadeem@gmail.com>
2018-04-29 07:20:11 -0700

    :tada: Initial commit

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,9 +3,11 @@
 # shinzui/grpc ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+  brew install cmake grpc protobuf
+  cargo install protobuf 
+  cargo install grpcio-compiler
+}
 
 # pkg.push() {
 #     git.push
